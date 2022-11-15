99e97a9435b9e0986ea20a3e9ba79d2d19bc42f6
ikave <shiftgear@mail.ru>
2022-11-15 15:12:09 +0300

    added base layout

---
+++ js/index.js
@@ -0,0 +1,19 @@
+// const menuToggleButton = document.querySelector('.menu-toggle');
+// const mobileMenu = document.querySelector('.mobile-menu');
+// const closeMenuButton = document.querySelector('.mobile-menu__close');
+
+// function closeMenu() {
+//   if (mobileMenu.classList.contains('mobile-menu--opened')) {
+//     document.body.style = null;
+//     mobileMenu.classList.remove('mobile-menu--opened');
+//     closeMenuButton.removeEventListener('click', closeMenu);
+//   }
+// }
+
+// function openMenu() {
+//   document.body.style.overflow = 'hidden';
+//   mobileMenu.classList.add('mobile-menu--opened');
+//   closeMenuButton.addEventListener('click', closeMenu);
+// }
+
+// menuToggleButton.addEventListener('click', openMenu);
