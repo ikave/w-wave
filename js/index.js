e7b7732bf1e60c9ea8bf1c7725b2165ae0b6f58f
ikave <shiftgear@mail.ru>
2022-11-25 13:42:08 +0300

    fix header

--- js/index.js
+++ js/index.js
@@ -17,3 +17,16 @@
 // }
 
 // menuToggleButton.addEventListener('click', openMenu);
+
+const search = document.querySelector('.search');
+const searchButton = search.querySelector('.search__button');
+const searchInput = search.querySelector('.search__input');
+
+searchButton.addEventListener('click', () => {
+  if (search.classList.contains('search--opened')) {
+    search.classList.remove('search--opened');
+  } else {
+    search.classList.add('search--opened');
+    searchInput.focus();
+  }
+});
